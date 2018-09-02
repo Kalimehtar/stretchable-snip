439213ab11ae887157414a8d274b21b2b1507812
Roman Klochkov <kalimehtar@mail.ru>
2018-09-02 23:34:12 +0500

    Initial commit

---
+++ info.rkt
@@ -0,0 +1,8 @@
+#lang info
+(define collection "stretchable-snip")
+(define deps '("base"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define scribblings '(("scribblings/stretchable-snip.scrbl" ())))
+(define pkg-desc "Stretchable snip% mixin")
+(define version "1.0")
+(define pkg-authors '(monk))
