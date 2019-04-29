a5e7681c9d689947db46a4ef08104e9fc2fb00c7
Roman Klochkov <kalimehtar@mail.ru>
2019-04-29 08:36:50 +0500

    Fix deps

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "stretchable-snip")
 (define deps '("base" "gui-lib"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "gui-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "draw-doc" "gui-doc"))
 (define scribblings '(("scribblings/stretchable-snip.scrbl" ())))
 (define pkg-desc "Stretchable snip% mixin")
 (define version "1.0")
